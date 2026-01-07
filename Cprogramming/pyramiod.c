c44aea5123cb898add1b5c6e7b4a7a30d0a640d8
sandeshmaharjan1 <sandeshmaharjan18@gmail.com>
2026-01-07 23:29:34 +0545

    made changes in pyramid.c

--- Cprogramming/pyramiod.c
+++ Cprogramming/pyramiod.c
@@ -1,21 +1,15 @@
 #include<stdio.h>
 
-int main() {
+int main(){
     int i, j, k, num;
 
     printf("Enter the number\n");
     scanf("%d", &num);
-
-    for(i=1; i<=num; i++){
-       
-        for(j=1; j<=i; j++){
-            
-            if (i %2 != 0)
-            {
+    for(i=num; i>=1; i--){
+        for(j=1;j<=i;j+2){
+            if(j%2 != 0){
                 printf("*");
             }
-            
-            
         }
         printf("\n");
     }
